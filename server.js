a2851395cdb2bac9b15791676e5f6eedbe4a0bca
Dharshana <dharshanans54@gmail.com>
2026-03-20 13:47:10 +0530

    cors issue fixed

--- server.js
+++ server.js
@@ -19,9 +19,14 @@ const corsOptions = {
   allowedHeaders: ['Content-Type', 'Authorization', 'ngrok-skip-browser-warning'],
 };
 app.use(cors(corsOptions));
-
-app.use(express.json());
+  app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Request Logger
+app.use((req, res, next) => {
+  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+  next();
+});
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
